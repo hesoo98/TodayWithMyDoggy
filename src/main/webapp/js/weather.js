a7bc8420d60ba3075a594a22ec253e819ff311ee
bohyun <lambdaramge@gmail.com>
2023-03-17 17:37:32 +0900

    Add weather

---
+++ src/main/webapp/js/weather.js
@@ -0,0 +1,58 @@
+const getJSON = function(url, callback) {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', url, true);
+  xhr.responseType = 'json';
+  xhr.onload = function() {
+    const status = xhr.status;
+    if(status === 200) {
+      callback(null, xhr.response);
+    } else {
+      callback(status, xhr.response);
+    }
+  };
+  xhr.send();
+};
+
+//현재 위치 불러오기
+var lat="";
+var lon="";
+navigator.geolocation.getCurrentPosition(function(pos){
+	console.log(pos);
+	
+	lat=Math.round(pos.coords.latitude,3);
+	lon=Math.round(pos.coords.longitude,3);
+})
+var api="e94b45e623f0fe23460ab9298bc2271b";
+//var url="https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid="+api;
+var url_ok="https://api.openweathermap.org/data/2.5/weather?q=seoul&appid="+api;
+
+getJSON(url_ok,function(err, data) {
+  if(err !== null) {
+    alert('날씨를 불러올수 없습니다' + err);
+  } else {
+	  var main=data.weather[0].main;
+	  var w_desc=data.weather[0].description;
+	  var temp=Math.round(data.main.temp- 273.15); //섭씨계산
+	  
+	  $("#w-temp").text(temp+"°");
+	  $("#w-desc").text(w_desc);
+	  $("#w-img").attr("src", "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
+	  
+	  if(temp>35 || temp<-10){
+		  $("#w-comment").text("오늘은 쉬어요!");
+	  }else{
+		  if(main=="Clear"){
+			  $("#w-comment").text("맑아서 산책하기 좋은 날씨에요!");
+		  }else if(main=="Clouds"){
+			  $("#w-comment").text("흐려서 산책하기 좋은 날씨에요!");
+		  }else if(main=="Rain" || main=="Drizzle"){
+			  $("#w-comment").text("비와도 산책할 수 있어요!");
+		  }else if(main=="Snow"){
+			  $("#w-comment").text("눈 보러가요!!!");
+		  }
+		  
+	  }
+  }
+  
+});
+
--- src/main/webapp/layout/main.jsp
+++ src/main/webapp/layout/main.jsp
@@ -6,8 +6,75 @@
 <meta charset="utf-8">
 <script src="https://code.jquery.com/jquery-3.6.3.js"></script>
 <title>Insert title here</title>
+
+<style type="text/css">
+
+	body{
+		border: 1px solid black;
+		width:100%;
+	}
+	
+	#wrapper{
+		text-align: center;
+	}
+	
+	.box {
+	  width: 400px;
+	  margin: 50px auto;
+	  border: 4px solid #d3d3d3;
+	  padding: 20px;
+	  text-align: center;
+	  font-weight: 700;
+	  color: #fdde6b;
+	  font-family: arial;
+	  position: relative;
+	}
+
+	.sb:before {
+	  content: "";
+	  width: 0px;
+	  height: 0px;
+	  position: absolute;
+	  border-left: 10px solid transparent;
+	  border-right: 10px solid transparent;
+	  border-top: 10px solid #d3d3d3;
+	  border-bottom: 10px solid transparent;
+	  right: 50%;
+	  bottom: -23px;
+	}
+	
+	.sb:after {
+	  content: "";
+	  width: 0px;
+	  height: 0px;
+	  position: absolute;
+	  border-left: 10px solid transparent;
+	  border-right: 10px solid transparent;
+	  border-top: 10px solid #fff;
+	  border-bottom: 10px solid transparent;
+	  right: 50%;
+	  bottom: -18px;
+	}
+
+</style>
+
 </head>
 <body>
-maintest
+
+	<div id="wrapper">
+		<div  class="box sb">
+		<span id="w-temp">온도</span>
+		<span id="w-desc">날씨</span>
+		<img id="w-img">
+		<br>
+		<span id="w-comment">문장</span>
+		</div>
+		<i class="fa-solid fa-dog fa-xl"></i>
+	</div>
+
+
 </body>
+
+<script type="text/javascript" src="js/weather.js"></script>
+<script src="https://kit.fontawesome.com/2663817d27.js" crossorigin="anonymous"></script>
 </html>
